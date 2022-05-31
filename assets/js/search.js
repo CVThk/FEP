2196aeb7629c1203e8d45d2a01e04cc82e192fe3
CVThk <thinhae2@gmail.com>
2022-05-31 15:58:13 +0700

    search input

---
+++ assets/js/search.js
@@ -0,0 +1,40 @@
+// var searchInput = document.getElementById('search__input');
+// searchInput.oninput = function(e) {
+//     console.clear()
+//     var productList = document.getElementsByClassName('product');
+//     for(var i = 0; i < productList.length; i++) {
+//         //console.log(searchInput.value)
+//         if(productList[i].querySelector('.product__caption-link').textContent.includes(searchInput.value)) {
+//             console.log(productList[i].querySelector('.product__caption-link').textContent)
+//         }
+//     }
+// }
+
+
+var searchInput = document.getElementById('search__input');
+var resultInput = document.querySelector('.result-search');
+var overPlay = document.querySelector('.overplay.search');
+
+overPlay.onclick = function() {
+    resultInput.style.display = 'none';
+    overPlay.style.display = 'none';
+}
+
+
+
+searchInput.oninput = function(e) {
+    var productList = document.getElementsByClassName('product');
+    resultInput.style.display = 'block';
+    overPlay.style.display = 'block';
+    var str = "";
+    for(var i = 0; i < productList.length; i++) {
+        var productCaptionLink = productList[i].querySelector('.product__caption-link');
+        if(productCaptionLink.textContent.includes(searchInput.value)) {
+            var productLink = productList[i].querySelector('.product__link');
+            var link = productLink.href;
+            str = str + '<a href="'+link+'">'+productCaptionLink.textContent+'</a>'
+            
+        }
+    }
+    resultInput.innerHTML = str;
+}
